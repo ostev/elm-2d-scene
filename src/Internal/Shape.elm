6243f04d2fb855a99a6d64d702282e66c7de933d
Oscar Stevens <oscar_stevens@icloud.com>
2021-09-28 05:31:38 +0000

    Remove commented-out code

--- src/Internal/Shape.elm
+++ src/Internal/Shape.elm
@@ -22,25 +22,4 @@ type Shape msg
 
 
 
--- toMesh : Shape msg -> WebGL.Mesh Vertex
--- toMesh =
---     toTriangles >> WebGL.triangles
--- {-| Extracts a [`Shape`'s](#Shape) texture,
--- if it has one. If it doesn't, [`Nothing`](Maybe#Nothing).
---     toTexture (image myTexture) == Just myTexture
---     toTexture rectangle
--- -}
--- toTexture : Shape msg -> Maybe Texture
--- toTexture shape =
---     case shape of
---         Textured texture _ ->
---             Just texture
---         _ ->
---             Nothing
--- toTriangles : Shape msg -> List ( Vertex, Vertex, Vertex )
--- toTriangles shape =
---     case shape of
---         Triangles triangles ->
---             triangles
---         Textured _ shape2 ->
---             toTriangles shape2
+
